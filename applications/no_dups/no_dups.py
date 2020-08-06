b0c74265f688d101623d34f17d5f0972d7d3492c
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-08-06 13:51:20 -0700

    no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,16 @@
 def no_dups(s):
-    # Your code here
+    cache = {}
+    str_list = s.split(" ")
+    result = []
+    for word in str_list:
+        if word not in cache:
+            cache[word] = True
+            result.append(word)
+    resultStr = " ".join(result)
+    return resultStr
 
+
+# no_dups("hello hello")
 
 
 if __name__ == "__main__":
